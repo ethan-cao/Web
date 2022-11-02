931ea4ad70f158b61c644b80db95736514cc7dca
ethan-cao <ethancaocgx@gmail.com>
2022-11-02 15:43:43 +0000

    3D rotation

--- WebGL/src/index.js
+++ WebGL/src/index.js
@@ -13,25 +13,100 @@ if (!gl) {
   throw('No webGL 2 support')
 }
 
+gl.enable(gl.DEPTH_TEST)
+
+gl.enable(gl.CULL_FACE)
+gl.frontFace(gl.CCW)
+gl.cullFace(gl.BACK)
+
+
 const vertexShader = await compileShader(gl, 'vertex', gl.VERTEX_SHADER)
 const fragmentShader = await compileShader(gl, 'fragment', gl.FRAGMENT_SHADER)
 const program = createProgram(gl, vertexShader, fragmentShader)
 
 
 // vertex data, accessible by CPU, but not GPU
-const vertices = [
-  //x,    y,    z,    R,    G,    B
-  0, 1, 0, 1.0, 1.0, 0.0,
-  1, -1, 0, 0.7, 0.0, 1.0,
-  -1, -1, 0, 0.1, 1.0, 0.6,
+const boxVertices = 
+[ // X, Y, Z           R, G, B
+  // Top
+  -1.0, 1.0, -1.0,   0.5, 0.5, 0.5,    // 0th vertex
+  -1.0, 1.0, 1.0,    0.5, 0.5, 0.5,    // 1st vertex
+  1.0, 1.0, 1.0,     0.5, 0.5, 0.5,    // 2nd vertex 
+  1.0, 1.0, -1.0,    0.5, 0.5, 0.5,
+
+  // Left
+  -1.0, 1.0, 1.0,    0.75, 0.25, 0.5,
+  -1.0, -1.0, 1.0,   0.75, 0.25, 0.5,
+  -1.0, -1.0, -1.0,  0.75, 0.25, 0.5,
+  -1.0, 1.0, -1.0,   0.75, 0.25, 0.5,
+
+  // Right
+  1.0, 1.0, 1.0,    0.25, 0.25, 0.75,
+  1.0, -1.0, 1.0,   0.25, 0.25, 0.75,
+  1.0, -1.0, -1.0,  0.25, 0.25, 0.75,
+  1.0, 1.0, -1.0,   0.25, 0.25, 0.75,
+
+  // Front
+  1.0, 1.0, 1.0,    1.0, 0.0, 0.15,
+  1.0, -1.0, 1.0,    1.0, 0.0, 0.15,
+  -1.0, -1.0, 1.0,    1.0, 0.0, 0.15,
+  -1.0, 1.0, 1.0,    1.0, 0.0, 0.15,
+
+  // Back
+  1.0, 1.0, -1.0,    0.0, 1.0, 0.15,
+  1.0, -1.0, -1.0,    0.0, 1.0, 0.15,
+  -1.0, -1.0, -1.0,    0.0, 1.0, 0.15,
+  -1.0, 1.0, -1.0,    0.0, 1.0, 0.15,
+
+  // Bottom
+  -1.0, -1.0, -1.0,   0.5, 0.5, 1.0,
+  -1.0, -1.0, 1.0,    0.5, 0.5, 1.0,
+  1.0, -1.0, 1.0,     0.5, 0.5, 1.0,
+  1.0, -1.0, -1.0,    0.5, 0.5, 1.0,
+];
+
+const boxIndices =
+[  
+  // number is index in boxVertices
+
+  // Top
+  0, 1, 2,     // the 0th, 1st, 2nd vertices form the 0th triangle
+  0, 2, 3,
+
+  // Left
+  5, 4, 6,
+  6, 4, 7,
+
+  // Right
+  8, 9, 10,
+  8, 10, 11,
+
+  // Front
+  13, 12, 14,
+  15, 14, 12,
+
+  // Back
+  16, 17, 18,
+  16, 18, 19,
+
+  // Bottom
+  21, 20, 22,
+  22, 20, 23
 ];
 
 // create buffer, accessible by GPU   
-const buffer = gl.createBuffer()
+const vertexBuffer = gl.createBuffer()
 // bind buffer to the bind point named gl.ARRAY_BUFFER, bind point is where data from CPU goes to GPU
-gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
 // send vertices in CPU to buffer in GPU through gl.ARRAY_BUFFER
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW)
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(boxVertices), gl.STATIC_DRAW)
+
+
+
+// order of triangle
+const indexBuffer = gl.createBuffer()
+gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer)
+gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(boxIndices), gl.STATIC_DRAW)
 
 
 // inform vertexShader how its attributes (input) gets value 
@@ -77,8 +152,8 @@ const projectionMatrix = new Float32Array(16)
 // worldMatrix handles rotation
 mat4.identity(worldMatrix) // generate identity matrix in worldMatrix
 // viewMatrix creates camera
-mat4.lookAt(viewMatrix, [0, 0, -4], [0, 0, 0], [0, 1, 0])
-mat4.perspective(projectionMatrix, glMatrix.toRadian(45), canvas.width / canvas.height, 0.1, 1000)
+mat4.lookAt(viewMatrix, [0, 0, -10], [0, 0, 0], [0, 1, 0])
+mat4.perspective(projectionMatrix, glMatrix.toRadian(45), canvas.clientWidth / canvas.clientHeight, 0.1, 1000.0);
 
 // send CPU accessible variables to shader
 gl.uniformMatrix4fv(worldMatrixLocation, gl.FALSE, worldMatrix)
@@ -86,21 +161,28 @@ gl.uniformMatrix4fv(viewMatrixLocation, gl.FALSE, viewMatrix)
 gl.uniformMatrix4fv(projectionMatrixLocation, gl.FALSE, projectionMatrix)
 
 
+const xRotationMatrix = new Float32Array(16)
+const yRotationMatrix = new Float32Array(16)
+
 const identityMatrix = new Float32Array(16)
 mat4.identity(identityMatrix)
 
+
 // main render loop
 const loop = () => {
+  // rotation
   const angle = performance.now() / 1000 / 6 * 2 * Math.PI  
-  mat4.rotate(worldMatrix, identityMatrix, angle, [0, 1, 0])
+  mat4.rotate(yRotationMatrix, identityMatrix, angle, [0, 1, 0])
+  mat4.rotate(xRotationMatrix, identityMatrix, angle / 4, [1, 0, 0])
+  mat4.mul(worldMatrix, xRotationMatrix, yRotationMatrix)
   gl.uniformMatrix4fv(worldMatrixLocation, gl.FALSE, worldMatrix)
 
   // clean previous draw
-  gl.clearColor(0.75, 0.85, 0.8, 1.0)
-  gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT)
+  gl.clearColor(0.75, 0.85, 0.8, 1.0);
+  gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
 
   // draw
-  gl.drawArrays(gl.TRIANGLES, 0, 3)
+  gl.drawElements(gl.TRIANGLES, boxIndices.length, gl.UNSIGNED_SHORT, 0);
   
   requestAnimationFrame(loop)
 }
